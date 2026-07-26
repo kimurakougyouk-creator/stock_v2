4a17608b147c138948df841ff6bbab1dd6dee90d
kimurakougyouk-creator <s.ossan777777@gmail.com>
2026-07-27 07:17:38 +0900

    Version 3.1: Integrate broker adapters with manager

--- tests/test_broker_manager.py
+++ tests/test_broker_manager.py
@@ -1,6 +1,7 @@
 import unittest
 
 from ai_asset_platform.brokers.manager import BrokerManager
+from ai_asset_platform.brokers.sbi_paper import SbiPaperAdapter
 
 
 class TestBrokerManager(unittest.TestCase):
@@ -16,8 +17,19 @@ class TestBrokerManager(unittest.TestCase):
     def test_get_all_returns_copy(self):
         brokers = self.manager.get_all()
         brokers.append("TEST")
-
         self.assertNotIn("TEST", self.manager.get_all())
+
+    def test_create_default_adapter(self):
+        adapter = self.manager.create_adapter()
+        self.assertIsInstance(adapter, SbiPaperAdapter)
+
+    def test_create_sbi_paper_adapter(self):
+        adapter = self.manager.create_adapter("SBI_PAPER")
+        self.assertIsInstance(adapter, SbiPaperAdapter)
+
+    def test_unsupported_broker(self):
+        with self.assertRaises(ValueError):
+            self.manager.create_adapter("UNKNOWN")
 
 
 if __name__ == "__main__":
