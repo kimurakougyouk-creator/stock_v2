3ad3518d2690b0aabad0fac1575a5416299acdf4
kimurakougyouk-creator <s.ossan777777@gmail.com>
2026-07-27 11:12:03 +0900

    Add safe AI judge foundation

---
+++ src/ai_asset_platform/ai/__init__.py
@@ -0,0 +1,13 @@
+from .ai_judge import (
+    AIJudgeResult,
+    AIProvider,
+    create_safe_fallback,
+    judge_with_ai,
+)
+
+__all__ = [
+    "AIJudgeResult",
+    "AIProvider",
+    "create_safe_fallback",
+    "judge_with_ai",
+]
