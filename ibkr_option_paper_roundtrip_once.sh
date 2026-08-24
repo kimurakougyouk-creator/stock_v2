4775caa30ef37cebcaa22071da0e4def7eb20ad5
kimurakougyouk-creator <kimurakougyouk@gmail.com>
2026-08-25 06:39:48 +0900

    Chain option Paper E2E into post-fill audit

--- ibkr_option_paper_roundtrip_once.sh
+++ ibkr_option_paper_roundtrip_once.sh
@@ -6,3 +6,4 @@ git pull --ff-only origin main
 source .venv/bin/activate
 python -m pytest -q
 AI_ASSET_ENABLE_IBKR_PAPER=1 IBKR_OPTION_E2E_CONFIRM="${IBKR_OPTION_E2E_CONFIRM:-}" python -m ai_asset_platform.brokers.ibkr_option_paper_roundtrip
+AI_ASSET_ENABLE_IBKR_PAPER=1 python -m ai_asset_platform.accounting.options_postfill_audit
