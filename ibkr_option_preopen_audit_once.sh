e9c8799ffe32b3d92939eb87829260762358e30e
kimurakougyouk-creator <kimurakougyouk@gmail.com>
2026-08-25 06:45:41 +0900

    Include What-If permission/spec check in preopen audit

--- ibkr_option_preopen_audit_once.sh
+++ ibkr_option_preopen_audit_once.sh
@@ -6,3 +6,4 @@ git pull --ff-only origin main
 source .venv/bin/activate
 python -m pytest -q
 AI_ASSET_ENABLE_IBKR_PAPER=1 python -m ai_asset_platform.brokers.ibkr_option_preopen_audit
+AI_ASSET_ENABLE_IBKR_PAPER=1 python -m ai_asset_platform.brokers.ibkr_option_permission_preflight
